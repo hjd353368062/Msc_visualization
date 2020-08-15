28e25e855f5037815f3997062f9b6483e72f8b8c
hjd353368062 <hjd353368062@gmail.com>
2020-08-15 06:09:55 +0100

    3

--- cgi-bin/getJSON.py
+++ cgi-bin/getJSON.py
@@ -3,11 +3,12 @@
 # enable debugging
 import cgitb
 import subprocess
+import json
 cmd = "python3 /home/mario/Desktop/mini-hoen/service_request.py request -j"
 proc = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 cgitb.enable()
 print('Content-Type: text/html')
 print()
-
+# a=json.dumps(proc.stdout.decode('utf-8'),indent=3)
 print(proc.stdout.decode('utf-8'))
 # print(proc.stderr)
